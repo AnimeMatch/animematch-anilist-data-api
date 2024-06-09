46b5f7f154e2901f07be77a51bff10b73048ff37
lucas.lrodrigues@sptech.school <lucas.lrodrigues@sptech.school>
2024-06-09 17:34:17 -0300

    Ajustando ips

--- src/main/java/teste/piProject/util/IPConfig.java
+++ src/main/java/teste/piProject/util/IPConfig.java
@@ -6,9 +6,9 @@ public class IPConfig {
     String ipApi = System.getenv("ENV_API");
     public String getIpApi() {
         if ("prod".equals(ipApi)) {
-            return "apo:8080";
+            return "apo:8080/apo";
         } else {
-            return "localhost:8080";
+            return "localhost:8080/apo";
         }
     }
 }
