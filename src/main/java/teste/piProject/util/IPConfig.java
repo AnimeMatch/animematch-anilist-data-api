ab187c3529ba8c6fba980b0eb69cc83a5ac0fdff
lucas.lrodrigues@sptech.school <lucas.lrodrigues@sptech.school>
2024-06-09 18:52:39 -0300

    removendo prefixo para teste

--- src/main/java/teste/piProject/util/IPConfig.java
+++ src/main/java/teste/piProject/util/IPConfig.java
@@ -6,9 +6,9 @@ public class IPConfig {
     String ipApi = System.getenv("ENV_API");
     public String getIpApi() {
         if ("prod".equals(ipApi)) {
-            return "apo:8080/apo";
+            return "apo:8080";
         } else {
-            return "localhost:8080/apo";
+            return "localhost:8080";
         }
     }
 }
